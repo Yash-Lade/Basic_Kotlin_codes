0e2874bd9cd8a867eb41ab6f1e3a46f0b5ca7a39
Yash Lade <150010042+Yash-Lade@users.noreply.github.com>
2024-03-06 14:46:32 +0530

    Kotlin program to add two numbers
    A Kotlin program to take the input as two numbers and produce the sum as an output of that numbers.

---
+++ Addition of two numbers.kt
@@ -0,0 +1,15 @@
+fun main()
+{
+    print("Enter first number :")
+    val input= readLine()
+    var num=input?.toIntOrNull()
+
+    print("Enter second number :")
+    val input1= readLine()
+    var num1=input1?.toIntOrNull()
+
+    if(num!=null && num1!=null) {
+        val sum = num1 + num
+        print("The sum is : $sum")
+    }
+}
